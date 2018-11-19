3728e2f6b9eeea7d4c1caaf3e4efb78d9a2e70c7
Tuomo Kriikkula <tuomo.kriikkula@student.oulu.fi>
2018-11-19 13:32:13 +0200

    Arduino project initialization

---
+++ cmake-build-debug/CMakeFiles/sensorduino.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "H:/Programming/sensorduino/src/main.cpp" "H:/Programming/sensorduino/cmake-build-debug/CMakeFiles/sensorduino.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/cores/arduino"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/variants/standard"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/EEPROM/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/HID/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/SPI/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/SoftwareSerial/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/Wire/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Adafruit_CircuitPlayground"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Adafruit_CircuitPlayground/utility"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Bridge/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Esplora/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Ethernet/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Firmata"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Firmata/utility"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/GSM/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Keyboard/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/LiquidCrystal/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Mouse/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/RobotIRremote/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Robot_Control/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Robot_Motor/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/SD/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Servo/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/SpacebrewYun/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Stepper/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/TFT/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/Temboo/src"
+  "C:/Users/tkrii_000/.platformio/packages/framework-arduinoavr/libraries/WiFi/src"
+  "C:/Users/tkrii_000/.platformio/packages/toolchain-atmelavr/avr/include"
+  "C:/Users/tkrii_000/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include"
+  "C:/Users/tkrii_000/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/5.4.0/include-fixed"
+  "C:/Users/tkrii_000/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sensorduino.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sensorduino.dir/src/main.cpp.obj"
+  "sensorduino.pdb"
+  "sensorduino.exe"
+  "sensorduino.exe.manifest"
+  "libsensorduino.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sensorduino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
